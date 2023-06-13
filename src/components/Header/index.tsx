8ffd81e57fbdcf004ce2861bc887065ed7f80174
Leandro Bernardo <lbsantos1@stefanini.com>
2023-06-13 09:21:17 -0300

    main header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,20 @@
-import todoLogo from '@assets/todoLogo.svg'
+import todoLogo from '../../assets/todoLogo.svg'
+import { AiOutlinePlusCircle } from 'react-icons/ai'
+
+import styles from './header.module.css'
 
 export function Header() {
   return (
-    <header>
+    <header className={styles.header}>
       <img src={todoLogo} alt="" />
+
+      <form className={styles.newTaskForm}>
+        <input placeholder="Adicione uma nova task" />
+        <button>
+          Criar
+          <AiOutlinePlusCircle size={20} />
+        </button>
+      </form>
     </header>
   )
 }
